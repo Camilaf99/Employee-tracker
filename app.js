d7d5a3648ce129789fc3c041d77c4a5e4c3b8bda
Camila Fernandez <ailenfernandez99@gmail.com>
2022-05-30 14:57:50 -0500

    added more questions, organized code

--- app.js
+++ app.js
@@ -13,204 +13,293 @@ const connection = mysql.createConnection({
     password: "Emca119!"
 });
 
-const viewChoices = [
-    "View Departments",
-    "View Roles",
-    "View Employees",
-    "Add Department",
-    "Add a Role",
-    "Add an Employee",
-    "Update Employee's Role",
-    "Exit"
-];
+function mainMenu() {
+    const viewChoices = [
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "Add Department",
+        "Add a Role",
+        "Add an Employee",
+        "Update Employee's Role",
+        "Exit"
+    ];
+    
+    inquirer.prompt([
+        {
+          name: "actions",
+          type: "list",
+          message: "What would you like to do?",
+          choices: viewChoices
+        },
+    ]).then(handleMainMenuOptions);
+}
 
-inquirer
-  .prompt([
-      {
-        name: "actions",
-        type: "list",
-        message: "What would you like to do?",
-        choices: viewChoices
-      },
-  ])
-  .then((answers) => {
-      switch (answers.actions) {
-          case "View Departments":
-              departments.getAllDepartments(connection, (err, result, fields) => {
-                  if(err) {
-                      console.error(err);
-                  } else {
-                    if(result) {
-                        const table = cTable.getTable(result);
-                        console.log(table);
-                    }
-                  }
-              });
-              break;
-          case "View Roles":
-            roles.getAllRoles(connection, (err, result, fields) => {
-                if(err) {
-                    console.error(err);
-                } else {
-                  if(result) {
-                      const table = cTable.getTable(result);
-                      console.log(table);
-                  }
-                }
-            });
-              break;
-          case "View Employees":
-            employees.getAllEmployees(connection, (err, result, fields) => {
-                if(err) {
-                    console.error(err);
-                } else {
-                  if(result) {
-                      const table = cTable.getTable(result);
-                      console.log(table);
-                  }
-                }
-            });
-              break;
-          case "Add Department":
-                inquirer.prompt ([
-                    {
-                        name: "department_name",
-                        type:"input",
-                        message:"Please provide name for the new department",
-                        validate: (s) => {
-                            if(s && !(s.trim() === "")) return true;
-                            else return "Please provide a valid department name";
-                        }
-                    }
-                ]).then((a) => {
-                    departments.addDepartment(connection, a.department_name, (err, result, field) => {
-                        if(err) {
-                            console.error(err);
-                        } else {
-                            console.log("Department added successfully");
-                        }
-                    });
-                });
-              break;
-          case "Add a Role":
-                inquirer.prompt ([
-                    {
-                        name: "role_name",
-                        type:"input",
-                        message:"Please provide name for the new role",
-                        validate: (s) => {
-                            if(s && !(s.trim() === "")) return true;
-                            else return "Please provide a valid role title";
-                        }
-                    },
-                    {
-                        name:"role_salary",
-                        type:"numerical",
-                        message:"Please provide role salary",
-                        validate: (v) => {
-                            if(v && v > 0) {
-                                return true;
-                            } else {
-                                return "Invalid salary range";
-                            }
-                        }
-                    },
-                    {
-                        name:"role_department",
-                        type:"numerical",
-                        message:"Please provide department id for new role",
-                        validate: (v) => {
-                            if(v && v > 0) {
-                                return true;
-                            } else {
-                                return "Invalid department id";
-                            }
-                        }
-                    }
-                ]).then((a) => {
-                    const role_title = a.role_name;
-                    const role_salary = a.role_salary;
-                    const role_dept = a.role_department;
+function viewAllDepartments() {
+    departments.getAllDepartments(connection, (err, result, fields) => {
+        if(err) {
+            console.error(err);
+        } else {
+          if(result) {
+              const table = cTable.getTable(result);
+              console.log(table);
+          }
+        }
+        mainMenu();
+    });
+}
 
-                    roles.addRole(connection,role_title,role_salary,role_dept, (err, result, fields) => {
-                        if(err) {
-                            console.error(err);
-                        } else {
-                            console.log("Role added successfully");
-                        }
-                    });
-                });
-              break;
-          case "Add an Employee":
-            inquirer.prompt ([
-                {
-                    name:"employee_firstname",
-                    type:"input",
-                    message:"Please provide employee first name",
-                    validate: (s) => {
-                        if(s && !(s.trim() === "")) return true;
-                        else return "Please provide a valid first name";
-                    }
-                },
-                {
-                    name:"employee_lastname",
-                    type:"input",
-                    message:"Please provide employee last name",
-                    validate: (s) => {
-                        if(s && !(s.trim() === "")) return true;
-                        else return "Please provide a valid last name";
-                    }
-                },
-                {
-                    name:"employee_role",
-                    type:"numerical",
-                    message:"Please provide employee role id",
-                    validate: (v) => {
-                        if(v && v > 0) {
-                            return true;
-                        } else {
-                            return "Invalid employee role id";
-                        }
-                    }
-                },
-                {
-                    name:"employee_manager",
-                    type:"numerical",
-                    message:"Please provide manager's id",
-                    default: null,
-                    validate: (v) => {
-                        if(!v || v > 0) {
-                            return true;
-                        } else {
-                            return "Invalid manager id";
-                        }
-                    }
+function viewAllRoles() {
+    roles.getAllRoles(connection, (err, result, fields) => {
+        if(err) {
+            console.error(err);
+        } else {
+          if(result) {
+              const table = cTable.getTable(result);
+              console.log(table);
+          }
+        }
+        mainMenu();
+    });
+}
+
+function viewAllEmployees() {
+    employees.getAllEmployees(connection, (err, result, fields) => {
+        if(err) {
+            console.error(err);
+        } else {
+          if(result) {
+              const table = cTable.getTable(result);
+              console.log(table);
+          }
+        }
+        mainMenu();
+    });
+}
+
+function addDepartment() {
+    inquirer.prompt ([
+        {
+            name: "department_name",
+            type:"input",
+            message:"Please provide name for the new department",
+            validate: (s) => {
+                if(s && !(s.trim() === "")) return true;
+                else return "Please provide a valid department name";
+            }
+        }
+    ]).then((a) => {
+        departments.addDepartment(connection, a.department_name, (err, result, field) => {
+            if(err) {
+                console.error(err);
+            } else {
+                console.log("Department added successfully");
+            }
+            mainMenu();
+        });
+    });
+}
+
+function addRole() {
+    inquirer.prompt ([
+        {
+            name: "role_name",
+            type:"input",
+            message:"Please provide name for the new role",
+            validate: (s) => {
+                if(s && !(s.trim() === "")) return true;
+                else return "Please provide a valid role title";
+            }
+        },
+        {
+            name:"role_salary",
+            type:"numerical",
+            message:"Please provide role salary",
+            validate: (v) => {
+                if(v && v > 0) {
+                    return true;
+                } else {
+                    return "Invalid salary range";
                 }
-            ]).then((a) => {
-                const fn = a.employee_firstname;
-                const ln = a.employee_lastname;
-                const er = a.employee_role;
-                const em = !a.employee_manager ? null : a.employee_manager;
-                employees.addEmployee(connection, fn, ln, er, em, (err, result, fields) => {
-                    if(err) {
-                        console.error(err);
-                    } else {
-                        console.log("Employee was created successfully");
-                    }
-                });
-            });
+            }
+        },
+        {
+            name:"role_department",
+            type:"numerical",
+            message:"Please provide department id for new role",
+            validate: (v) => {
+                if(v && v > 0) {
+                    return true;
+                } else {
+                    return "Invalid department id";
+                }
+            }
+        }
+    ]).then((a) => {
+        const role_title = a.role_name;
+        const role_salary = a.role_salary;
+        const role_dept = a.role_department;
+
+        roles.addRole(connection,role_title,role_salary,role_dept, (err, result, fields) => {
+            if(err) {
+                console.error(err);
+            } else {
+                console.log("Role added successfully");
+            }
+            mainMenu();
+        });
+    });
+}
+
+function addEmployee() {
+    inquirer.prompt ([
+        {
+            name:"employee_firstname",
+            type:"input",
+            message:"Please provide employee first name",
+            validate: (s) => {
+                if(s && !(s.trim() === "")) return true;
+                else return "Please provide a valid first name";
+            }
+        },
+        {
+            name:"employee_lastname",
+            type:"input",
+            message:"Please provide employee last name",
+            validate: (s) => {
+                if(s && !(s.trim() === "")) return true;
+                else return "Please provide a valid last name";
+            }
+        },
+        {
+            name:"employee_role",
+            type:"numerical",
+            message:"Please provide employee role id",
+            validate: (v) => {
+                if(v && v > 0) {
+                    return true;
+                } else {
+                    return "Invalid employee role id";
+                }
+            }
+        },
+        {
+            name:"employee_manager",
+            type:"numerical",
+            message:"Please provide manager's id",
+            default: null,
+            validate: (v) => {
+                if(!v || v > 0) {
+                    return true;
+                } else {
+                    return "Invalid manager id";
+                }
+            }
+        }
+    ]).then((a) => {
+        const fn = a.employee_firstname;
+        const ln = a.employee_lastname;
+        const er = a.employee_role;
+        const em = !a.employee_manager ? null : a.employee_manager;
+        employees.addEmployee(connection, fn, ln, er, em, (err, result, fields) => {
+            if(err) {
+                console.error(err);
+            } else {
+                console.log("Employee was created successfully");
+            }
+            mainMenu();
+        });
+    });
+}
+
+function updateEmployeeRole() {
+    inquirer.prompt ([
+        {
+            name:"update_employee",
+            type:"numerical",
+            message:"Please provide employee id",
+            validate: (v) => {
+                if(v && v > 0) {
+                    return true;
+                } else {
+                    return "Invalid employee id";
+                }
+            }
+        },
+        {
+            name:"update_role",
+            type:"input",
+            message:"Please provide employee new role",
+            validate: (v) => {
+                if(v && v > 0) {
+                    return true;
+                } else {
+                    return "Invalid employee role id";
+                }
+            }
+        }
+    ]).then((a)=> {
+        const eid = a.update_employee;
+        const rid = a.update_role;
+
+        employees.updateEmployeeRole(connection, rid, eid, (err, result, fields) => {
+            if(err) {
+                console.error(err);
+            } else {
+                console.log("Employee was updated successfully");
+            }
+            mainMenu();
+        });
+    });
+}
+
+function handleExit() {
+    inquirer.prompt([
+        {
+            name: "confirmExit",
+            type: "confirm",
+            message: "Are you sure you want to quit?"
+        }
+    ]).then((a) => {
+        if(a.confirmExit) {
+            process.exit(0);
+        } else {
+            mainMenu();
+        }
+    });
+}
+
+function handleMainMenuOptions(answers) {
+    switch (answers.actions) {
+        case "View Departments":
+            viewAllDepartments();
             break;
-          case "Update Employee's Role":
-              console.log("I want to update an employee's role");
-              break;
-          case "Exit":
-          default:
-              console.log("Exit");
-      }
-//    console.log(answers.actions);
-  });
+        case "View Roles":
+            viewAllRoles();
+            break;
+        case "View Employees":
+            viewAllEmployees();
+            break;
+        case "Add Department":
+            addDepartment();
+            break;
+        case "Add a Role":
+            addRole();
+            break;
+        case "Add an Employee":
+            addEmployee();
+            break;
+        case "Update Employee's Role":
+            updateEmployeeRole();
+            break;
+        case "Exit":
+        default:
+            handleExit();
+    }
+}
 
 
+mainMenu();
 
 /*
 const viewEmployees = [
@@ -237,44 +326,3 @@ const employeeRoles = [
 
 */
 
-/*   
-     inquirer.prompt ([
-         {
-             name:"employee_firstname",
-             type:"input",
-             message:"Please provide employee first name"
-         },
-         {
-             name:"employee_lastname",
-             type:"input",
-             message:"Please provide employee last name"
-         },
-         {
-             name:"employee_role",
-             type:"input",
-             message:"Please provide employee role"
-         },
-         {
-             name:"employee_manager",
-             type:"input",
-             message:"Please provide manager's id"
-         }
-     ]);
-
-     inquirer
-     .prompt ([
-         {
-             name:"update_employee",
-             type:"input",
-             message:"Please provide employee id"
-         }
-     ]);
-
-     inquirer
-     .prompt ([
-         {
-             name:"update_role",
-             type:"input",
-             message:"Please provide employee new role"
-         }
-     ]); */
--- services/employeeService.js
+++ services/employeeService.js
@@ -1,3 +1,5 @@
+const { up } = require("inquirer/lib/utils/readline");
+
 const getAllEmployees = (connection, callback) => {
     connection.query('select e.id, e.first_name, e.last_name, e.role_id, r.title, e.manager_id from employees e, roles r where e.role_id = r.id', callback);
 };
@@ -6,5 +8,10 @@ const addEmployee = (connection, first_name, last_name, employee_role, employee_
     connection.execute('insert into employees (first_name, last_name, role_id, manager_id) values (?,?,?,?)',[first_name, last_name, employee_role, employee_manager], callback);
 };
 
+const updateEmployeeRole = (connection, role_id, employee_id,callback) => {
+    connection.execute('update employees set role_id = ? where employees.id = ?;',[role_id,employee_id], callback);
+};
+
 exports.getAllEmployees = getAllEmployees;
 exports.addEmployee = addEmployee;
+exports.updateEmployeeRole = updateEmployeeRole;
